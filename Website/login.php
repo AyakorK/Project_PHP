0edb3c36e6fafd1a1c07dcad913641041c7a887d
Alan <91255496+alan190902@users.noreply.github.com>
2022-01-24 15:33:19 +0100

    Update login.php

--- Website/login.php
+++ Website/login.php
@@ -1,21 +1,24 @@
 <?php
 
 ?>
-    <!DOCTYPE html>
-    <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Titre de la page</title>
-        <link rel="stylesheet" href="style.css">
-        <script src="script.js"></script>
-    </head>
-    <body>
-    <form class="login">
-        <label> Utilisateur </label>
-        <input type="text" name="user" id="user">
-        <label> Mot de passe </label>
-        <input type="password" name="password" id="password">
-        <button class="Bconnect"> Connexion </button>
-    </form>
-    </body>
-    </html>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+<form method="GET" action="login.php">
+    <p>
+        <span>Login</span>
+    </p>
+    <label> Utilisateur </label>
+    <input type="text" name="user" id="user">
+    <label> Mot de passe </label>
+    <input type="password" name="password" id="password">
+    <input type="submit" value="Connexion">
+</form>
+</body>
+</html>
