57677372f4c71c087756d9751c098ede8102317f
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-01-24 16:47:31 +0100

    Adding a style to some pages

---
+++ Website/index.php
@@ -0,0 +1,21 @@
+<?php
+
+  
+
+?>
+<!DOCTYPE html>
+    <html lang="fr">
+      <head>
+        <meta charset="utf-8">
+        <title>Petiot Comptable</title>
+        <link rel="stylesheet" href="assets/css/homepage.css">
+        
+      </head>
+      <body>
+        <h1> Petit Comptable</h1>
+        <h2>Bienvenue sur le site de petit comptable</h2>      
+        <button class="connect" onclick="window.location.href='login.php'">Se connecter</button>
+        <button class="register" onclick="window.location.href='register.php'">S'inscrire</button>
+      </body>
+      <script src="script.js"></script>
+    </html>
