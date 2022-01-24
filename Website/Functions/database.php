aba893935539fe27c67c10e04a8875fb0d6e5707
ilan <ilan.petiot@icloud.com>
2022-01-24 21:20:58 +0100

    increment code

--- Website/Functions/database.php
+++ Website/Functions/database.php
@@ -1,21 +1,29 @@
 <?php
 
- function dbConnect() {
+function dbConnect(){
     try {
-    $host = 'mysql-mkpdphp.alwaysdata.net';
-    $dbname = 'mkpdphp_petiotcomptable';
-    $user = 'mkpdphp';
-    $pass = 'mkpdphpcompta';
+        $host = 'mysql-mkpdphp.alwaysdata.net';
+        $dbname = 'mkpdphp_petiotcomptable';
+        $user = 'mkpdphp';
+        $pass = 'mkpdphpcompta';
 
 
-    $dataBase =  new PDO("mysql:host=$host;dbname=$dbname",
-    $user,
-    $pass,
-    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::MYSQL_ATTR_DIRECT_QUERY => true));
-    return $dataBase;
+        $dataBase = new PDO("mysql:host=$host;dbname=$dbname",
+            $user,
+            $pass,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::MYSQL_ATTR_DIRECT_QUERY => true));
+        return $dataBase;
     } catch (PDOException $e) {
-    echo 'Connexion échouée : ' . $e->getMessage();
+        echo 'Connexion échouée : ' . $e->getMessage();
     }
 }
 
-?>
+//$db = dbConnect();
+//
+////Préparation de la requête sql
+//$req = $db->prepare("SELECT * FROM User WHERE emailUser = 'baptistest@gmail.com'");
+//$req->execute(array("emailUser"=>$_POST['emailUser']));
+//
+//while ($data = $req->fetch()){
+//    echo htmlspecialchars($data['emailUser']);
+//}
