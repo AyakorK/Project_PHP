acbf4bd81229019dadcd909adb84564a623d538b
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-01-24 13:58:05 +0100

    Index / Login / Register HTML Structure
    The base of the HTML code

---
+++ Website/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+      <meta charset="utf-8">
+      <title>Titre de la page</title>
+      <link rel="stylesheet" href="style.css">
+      <script src="script.js"></script>
+    </head>
+    <body>
+    <h1> Petit Comptable</h1>
+    <h2>Bienvenue sur le site de petit comptable</h2>
+    <button class="connect" onclick="window.location.href='login.php'">Se connecter</button>
+    <button class="register" onclick="window.location.href='register.php'">S'inscrire</button>
+    </body>
+    </html>
