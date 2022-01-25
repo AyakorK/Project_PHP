0553ba5983cb06e8703b1dde0c1e96afc26856f6
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2022-01-25 16:05:31 +0100

    FIX Delete Account

--- Website/Functions/deleteAccount.php
+++ Website/Functions/deleteAccount.php
@@ -4,15 +4,16 @@ session_start();
 include_once 'database.php';
 $db = dbConnect();
 
-$email = $_SESSION['email'];
+// $email = $_SESSION['email'];
+$userID = $_SESSION['actualUserID'];
 
 if (isset($_POST['deleteAccount'])) {
 
-    $query = $db->prepare("DELETE FROM User WHERE emailUser = :email");
-    $query->bindParam(':email', $email);
+    $query = $db->prepare("DELETE FROM User WHERE userID = :userID");
+    $query->bindParam(':userID', $userID);
     $query->execute();
 
     echo "<script>alert('Account has been deleted.');</script>";
-    header( "Refresh: 0.5s; url=../index.php" ) ;
+    header( "Refresh: 0.5; url=../index.php" ) ;
 
 }
