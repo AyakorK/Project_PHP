32e77e53d18d296b99d17e83356028fdf0fc7e9e
ilan <ilan.petiot@icloud.com>
2022-01-25 15:49:49 +0100

    ✏️ - Replace userID to actualUserID

--- Website/Functions/insertBankAccount.php
+++ Website/Functions/insertBankAccount.php
@@ -3,7 +3,8 @@
 require_once 'database.php';
 function insertBankAccount()
 {
-
+    session_start();
+    $actualUserID = $_SESSION['actualUserID'];
 
     if (isset($_POST['submitBankAccount'])) {
         $db = dbConnect();
@@ -15,19 +16,20 @@ function insertBankAccount()
 
 
         //Préparation de la requête sql
-        $req = $db->query("SELECT COUNT(*) FROM bankAccount WHERE userId = 5")->fetchColumn();
+        $req = $db->query("SELECT COUNT(*) FROM bankAccount WHERE userId = $actualUserID")->fetchColumn();
+
         var_dump('avant '. $req);
 
         if($req < 10){
             $req = $db->prepare('INSERT INTO bankAccount (userID, accountName, accountType, soldAccount, currency) VALUES (:userID, :accountName , :accountType, :soldAccount, :currency)');
             $req->execute(array(
-                "userID" => 5,
+                "userID" => $actualUserID,
                 "accountName" => $accName,
                 "accountType" => $accType,
                 "soldAccount" => $SoldAccount,
                 "currency" => $accCurrency));
             echo 'fini';
-            var_dump('après '. $req);
+
         }else{
             echo 'marche pas';
         }
