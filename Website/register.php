6f84508ec78bdac731886311997ced950d77e646
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-01-26 23:30:38 +0100

    Adding some CSS

--- Website/register.php
+++ Website/register.php
@@ -3,17 +3,33 @@
     <head>
         <meta charset="utf-8">
         <title>Titre de la page</title>
-        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="assets/css/register.css">
         <script src="script.js"></script>
     </head>
+
     <body>
-    <form method="POST" action="Functions/insertRegister.php">
+        <!-- Faire un header contenant le logo de notre entreprise -->
+        <header>
+           
+                <img  id="logo" src="src/logo.png" alt="logo">
+                <p>PetiotComptable</p>
+                <img id="user" src="src/user.png" alt="user">
+                
+        
+        </header>
+    <form class="thisForm" method="POST" action="Functions/insertRegister.php">
+    <div class="formText">
         <label> E-mail </label>
-        <input type="text" name="email" id="email" placeholder="Entrez votre email">
+        <input type="text" name="email" id="email" placeholder="Enter e-mail...">
+    </div>
+    <div class="formText">
         <label> Password </label>
-        <input type="password" name="password" id="password" placeholder="Entrez votre mot de passe">
+        <input  type="password" name="password" id="password" placeholder="Enter password...">
+    </div>
+    <div class="formText">
         <label> Confirm password </label>
-        <input type="password" name="password2" id="password2" placeholder="Confirmez votre mot de passe">
+        <input  type="password" name="password2" id="password2" placeholder="Confirm password...">
+    </div>
         <button class="BRegister" name="submitRegister" id="submitRegister"> Register </button>
     </form>
     </body>
