7c39b457402ecc0f977a3bdc0b10e366fcd4667b
ilan <ilan.petiot@icloud.com>
2022-01-26 20:29:33 +0100

    Move all function in bankAccountFunctions.php and call it

--- Website/modifyBankAccount.php
+++ Website/modifyBankAccount.php
@@ -1,57 +1,7 @@
 <?php
     session_start();
-    require_once 'Functions/database.php';
-    $db = dbConnect();
-    $actualBankID = $_SESSION['actualBankID'];
-
-    // Find in database all values for actual bankAccount
-    $req = $db->prepare("SELECT * FROM bankAccount WHERE accountID = :bankAccountID");
-    $req->execute(array(":bankAccountID"=>$actualBankID));
-
-    // Set each values on session
-    while ($data = $req->fetch()){
-
-        $_SESSION['actualAccountName'] = htmlspecialchars($data['accountName']);
-        $_SESSION['actualAccountType'] = htmlspecialchars($data['accountType']);
-        $_SESSION['actualSoldAccount'] = htmlspecialchars($data['soldAccount']);
-        $_SESSION['actualCurrency'] = htmlspecialchars($data['currency']);
-    }
-    ////////
-
-    function accountType()
-    {
-        if ($_SESSION['actualAccountType'] == 'Courant') {
-
-            echo '<option value="courant" selected>Courant</option>';
-            echo '<option value="epargne">Epargne</option>';
-            echo '<option value="compteJoint">Compte joint</option>';
-
-        } else if($_SESSION['actualAccountType'] == 'Epargne'){
-
-            echo '<option value="courant">Courant</option>';
-            echo '<option value="epargne" selected>Epargne</option>';
-            echo '<option value="compteJoint">Compte joint</option>';
-
-        } else if($_SESSION['actualAccountType'] == 'compteJoint'){
-
-            echo '<option value="courant">Courant</option>';
-            echo '<option value="epargne">Epargne</option>';
-            echo '<option value="compteJoint" selected>Compte joint</option>';
-        }
-    }
-
-    function accountCurrency(){
-            if($_SESSION['actualCurrency'] == 'EUR'){
-
-                echo '<option value="eur">EUR</option>';
-                echo '<option value="usd">USD</option>';
-
-            } else if($_SESSION['actualCurrency'] == 'USD'){
-
-                echo '<option value="usd">USD</option>';
-                echo '<option value="eur">EUR</option>';
-            }
-    }
+    require_once 'Functions/bankAccountFunctions.php';
+    knowBankData();
 ?>
 
 
@@ -61,7 +11,6 @@
     <meta charset="utf-8">
     <title>Create Bank Account</title>
     <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
 </head>
 <body>
 <form method="POST" action="Functions/uptadeBankAccount.php">
