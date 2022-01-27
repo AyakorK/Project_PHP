aac9813d609d84175295d79270639aa31c8acd6a
ilan <ilan.petiot@icloud.com>
2022-01-27 13:20:42 +0100

    ✨ - Add condition if the user enter or not values in input

--- Website/Functions/insertBankAccount.php
+++ Website/Functions/insertBankAccount.php
@@ -21,56 +21,59 @@ function insertBankAccount()
         $SoldAccount = testInput($SoldAccount);
         $accCurrency = testInput($accCurrency);
 
-        if(filter_var($SoldAccount, FILTER_VALIDATE_INT)){
-            echo "<script>alert('good');</script>";
-            header("Refresh: .5; url=../homepage.php");
-        }
 
         // Check if all input are valid
         if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/',$accName) or preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/',$accType) or preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/',$SoldAccount) or preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/',$accCurrency)){
 
             echo "<script>alert('This bank account is not valid, Special characters not allowed');</script>";
             header("Refresh: .5; url=../homepage.php");
-
         }
         else {
 
-            // Check if accType and accCurrency is correct
-            if(($accType == 'courant' or $accType == 'epargne' or $accType == 'compteJoint') and ($accCurrency == 'usd' or $accCurrency == 'eur')){
+            //  Check if all input aren't empty
+            if(!empty($_POST['accountName']) and !empty($_POST['accountSold'])){
 
-                // Check if soldAccount is an Integer
-                if(filter_var($SoldAccount, FILTER_VALIDATE_INT)){
+                // Check if accType and accCurrency is correct
+                if(($accType == 'courant' or $accType == 'epargne' or $accType == 'compteJoint') and ($accCurrency == 'usd' or $accCurrency == 'eur')){
 
-                    //  Sum of all userID banks
-                    $req = $db->query("SELECT COUNT(*) FROM bankAccount WHERE userId = $actualUserID")->fetchColumn();
+                    // Check if soldAccount is an Integer
+                    if(filter_var($SoldAccount, FILTER_VALIDATE_INT)){
+
+                        //  Sum of all userID banks
+                        $req = $db->query("SELECT COUNT(*) FROM bankAccount WHERE userId = $actualUserID")->fetchColumn();
 
 
-                    //  To avoid user have more than 10 bank account
-                    if ($req < 10) {
-                        //  Insert values in database
-                        $req = $db->prepare('INSERT INTO bankAccount (userID, accountName, accountType, soldAccount, currency) VALUES (:userID, :accountName , :accountType, :soldAccount, :currency)');
-                        $req->execute(array(
-                            "userID" => $actualUserID,
-                            "accountName" => $accName,
-                            "accountType" => $accType,
-                            "soldAccount" => $SoldAccount,
-                            "currency" => $accCurrency));
-                        echo "<script>alert('" . $accName . " has been created');</script>";
-                        header("Refresh: .5; url=../homepage.php");
+                        //  To avoid user have more than 10 bank account
+                        if ($req < 10) {
+                            //  Insert values in database
+                            $req = $db->prepare('INSERT INTO bankAccount (userID, accountName, accountType, soldAccount, currency) VALUES (:userID, :accountName , :accountType, :soldAccount, :currency)');
+                            $req->execute(array(
+                                "userID" => $actualUserID,
+                                "accountName" => $accName,
+                                "accountType" => $accType,
+                                "soldAccount" => $SoldAccount,
+                                "currency" => $accCurrency));
+                            echo "<script>alert('" . $accName . " has been created');</script>";
+                            header("Refresh: .5; url=../homepage.php");
 
-                    }
-                    else {
-                        echo "<script>alert('" . $accName . " has been declined');</script>";
+                        }
+                        else {
+                            echo "<script>alert('" . $accName . " has been declined');</script>";
+                            header("Refresh: .5; url=../homepage.php");
+                        }
+                    }else{
+                        echo "<script>alert('Your Account Sold is not valid, Please try again');</script>";
                         header("Refresh: .5; url=../homepage.php");
                     }
                 }else{
-                    echo "<script>alert('Your Account Sold is not valid, Please try again');</script>";
+                    echo "<script>alert('This bank account is not valid, unknown account type or account currency');</script>";
                     header("Refresh: .5; url=../homepage.php");
                 }
             }else{
-                echo "<script>alert('This bank account is not valid, unknown account type or account currency');</script>";
+                echo "<script>alert('Bank account name or account sold is empty, Please try again');</script>";
                 header("Refresh: .5; url=../homepage.php");
             }
+
         }
 
     }
