00bf54e48458660331f7559ec1ac94dd209bb28b
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2022-01-27 17:41:45 +0100

    Update goToBankAccount.php

--- Website/Functions/goToBankAccount.php
+++ Website/Functions/goToBankAccount.php
@@ -23,7 +23,7 @@ knowBankData();
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <title>Homepage</title>
+    <title>Bank Account</title>
     <link rel="stylesheet" href="../assets/css/header.css">
         <link rel="stylesheet" href="../assets/css/form.css">
         <link rel="stylesheet" href="../assets/css/bankAccount.css">
